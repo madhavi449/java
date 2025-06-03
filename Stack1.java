3c96fd33ae242fc7b9776075e6aeaddb624f4cbe
madhavi449 <madhavisagilereddy344@gmail.com>
2025-06-03 15:50:23 +0530

    Add files via upload

---
+++ Stack1.java
@@ -0,0 +1,10 @@
+import java.util.Stack1;
+public class Main10{
+    public static void main(String[] args){
+        Stack s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        System.out.print(s.peek());
+        System.out.print(s.pop());
+    }
+}
