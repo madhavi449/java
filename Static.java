d888430015bded85bb3bfede7489fc25e1708973
madhavi449 <madhavisagilereddy344@gmail.com>
2025-06-06 15:07:21 +0530

    Add files via upload

---
+++ Static.java
@@ -0,0 +1,12 @@
+public class Static{
+    public static void add()
+    {
+        int a = 8;
+        int b = 5;
+        int c = a+b;
+        System.out.println(c);
+    }
+        public static void main(String[] args){
+            add();
+        }
+}
