71b48ab283cc7001a3ce5aa4f8cc063e93443188
madhavi449 <madhavisagilereddy344@gmail.com>
2025-06-09 15:14:50 +0530

    Add files via upload

---
+++ Recursive1.java
@@ -0,0 +1,15 @@
+ public class Recursive1
+     {
+        public void fun(int n)
+        {
+        if(n==11)return;
+        System.out.println(n);
+        fun(n+1);
+        System.out.println(n);
+        } 
+        public static void main(String[] args){
+        Main m=new Main();
+        m.fun(1);
+        }
+     }
+ 
